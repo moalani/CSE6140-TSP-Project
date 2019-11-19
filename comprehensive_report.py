8c15caf7308b4e9986d86fcb50fc21d94c4686ef
Justin Bozonier <darkxanthos@gmail.com>
2019-11-19 06:34:16 -0600

    Integrated with BnB and ran comprehensive report.

--- comprehensive_report.py
+++ comprehensive_report.py
@@ -1,7 +1,8 @@
 import glob
 import genetic_algorithm
+import BnB
 import datetime as dt
-from utilities import load_data
+import utilities
 import json
 from tracer import NullTracer
 
@@ -9,18 +10,18 @@ from tracer import NullTracer
 if __name__ == '__main__':
     runs_per_instance = 15
     results = {}
-    for city_file in glob.glob('DATA/*.tsp'): #['DATA/UKansasState.tsp', 'DATA/Atlanta.tsp']:#
+    for city_file in glob.glob('DATA/*.tsp'):
         print(city_file)
         for i in range(runs_per_instance):
             print(i)
-            instance_name, city_data = load_data(city_file)
+            instance_name, city_data = utilities.load_data(city_file)
             start_time = dt.datetime.now()
-            score, solution = genetic_algorithm.solve(data=city_data,
-                                                      timer=lambda: (dt.datetime.now() - start_time).total_seconds() < 600,
+            score, solution = BnB.solve(data=city_data,
+                                                      timer=utilities.early_stop_checker(600),
                                                       tracer=NullTracer())
             end_time = dt.datetime.now()
             instance_results = results.get(instance_name, {'costs': [], 'times': []})
-            instance_results['costs'].append(score)
+            instance_results['costs'].append(int(score))
             instance_results['times'].append((end_time - start_time).total_seconds())
             results[instance_name] = instance_results
-    json.dump(obj=results, fp=open('comprehensive_report.json', 'w'))
+    json.dump(obj=results, fp=open('comprehensive_report_bnb.json', 'w'))
